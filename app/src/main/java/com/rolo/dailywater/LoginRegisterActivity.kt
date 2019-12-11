b9fb16b5e09729158cea54dc06e340113399aee8
rolo7 <50997752+rolo7@users.noreply.github.com>
2019-12-11 13:32:49 -0600

    Initial commit

---
+++ app/src/main/java/com/rolo/dailywater/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.rolo.dailywater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
